70ccf64dbfd6e27edbdb07ad41a0ce253ceb0cad
Guga <contato.gustavobarbosa@outlook.com>
2021-09-20 14:23:40 -0300

    Preparando revisão do Yohan

--- src/pages/Main/components/HabitsView/style.js
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components';
-
-export const AddHabitMenu = styled.div`
-    width: 100%;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-`
